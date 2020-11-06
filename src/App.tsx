299a796d532f123427cf279a0690a212772606ae
erobert <etiennerobert33@gmail.com>
2020-11-06 23:45:36 +0100

    add a list of removable items

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
+const generateArray = (n: number) => Array.from(Array(n).keys());
+
 function App() {
+  const [list, setList] = useState(generateArray(50));
+
+  const deleteItem = (i: number) => {
+    setList((list) => list.filter((v) => v != i));
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      {list.map((item) => (
+        <div className="item" key={item} onClick={() => deleteItem(item)}>
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
